1fddaed66c276a3a7ea0f7707b4c3ec3ad576741
rrayachhetri <rrayachhetri@gmail.com>
2022-10-21 09:55:37 -0700

    created getStat() and getInventory() methods using prototype

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -5,7 +5,7 @@ jest.mock('../lib/Potion');
 
 console.log(new Potion());
 
-test('create a player object',()=>{
+test('create a player object', () => {
     const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
@@ -18,4 +18,22 @@ test('create a player object',()=>{
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stat as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+     const player = new Player('Dave');
+
+     expect(player.getInventory()).toEqual(expect.any(Array));
+     player.inventory = [];
+     
+     expect(player.getInventory()).toEqual(false);
 });
